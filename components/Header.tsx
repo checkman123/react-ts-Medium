869696f3cd492a0962bcdc031e26e1ccb640b7d8
Nez <sanyapoom.siri@gmail.com>
2022-10-03 19:08:10 -0700

    create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import React from "react";
+import logo from "../public/reddit-logo.png";
+import {
+  ArrowRightOnRectangleIcon,
+  Bars3Icon,
+  ChevronDownIcon,
+  HomeIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/solid";
+import {
+  ChatBubbleLeftEllipsisIcon,
+  BellIcon,
+  VideoCameraIcon,
+  GlobeAltIcon,
+  SparklesIcon,
+  PlusIcon,
+  SpeakerWaveIcon,
+} from "@heroicons/react/24/outline";
+
+export const Header = () => {
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      {/* Logo */}
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image objectFit="contain" src={logo} layout="fill" />
+      </div>
+      {/* Home */}
+      <div className="mx-7 flex items-center gap-x-2 xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+      {/* Search Bar */}
+      <form className="flex flex-1 items-center rounded-sm border border-gray-200 bg-gray-100 px-3 py-1 ">
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+      {/* Menu - hidden but show on large screen (lg:inline-flex)*/}
+      <div className="mx-5 hidden items-center gap-2 text-gray-500 lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeAltIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatBubbleLeftEllipsisIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerWaveIcon className="icon" />
+      </div>
+      {/* Hidden on large screen */}
+      <div className="ml-5 flex items-center lg:hidden">
+        <Bars3Icon className="icon" />
+      </div>
+
+      {/*Sign In/Out Button */}
+      <div className="hidden cursor-pointer items-center gap-x-2 border border-gray-100 lg:flex">
+        <ArrowRightOnRectangleIcon className="icon flex-shrink-0" />
+        <p className="text-gray-500">Sign In</p>
+      </div>
+    </div>
+  );
+};
